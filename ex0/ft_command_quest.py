a58f4ae305f2d398a4c87c4e40530c244bfc382d
MusangQim <musangqim03@gmail.com>
2026-04-09 21:58:48 +0800

    update condition for argument counting

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -4,4 +4,8 @@ import sys
 input_user = len(sys.argv)
 print("=== Command Quest ===")
 print(f"Program name: {sys.argv[0]}")
+if len(sys.argv) > 1:
+    print(f"Arguments received: {input_user}")
+else:
+    print("No argument provided!")
 print(f"Total arguments: {input_user}")
