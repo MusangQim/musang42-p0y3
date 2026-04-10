51e768d9845d3845af9e0ce45f5a4c91170befcd
MusangQim <musangqim03@gmail.com>
2026-04-10 11:11:17 +0800

    update ex0

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -5,7 +5,10 @@ input_user = len(sys.argv)
 print("=== Command Quest ===")
 print(f"Program name: {sys.argv[0]}")
 if len(sys.argv) > 1:
-    print(f"Arguments received: {input_user}")
+    input_receive = input_user - 1
+    print(f"Arguments received: {input_receive}")
+    for input_list in range(1, len(sys.argv)):
+        print(f"Argument {input_list} : {sys.argv[input_list]}")
 else:
     print("No argument provided!")
 print(f"Total arguments: {input_user}")
