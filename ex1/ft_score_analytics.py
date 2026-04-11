c672a34f3ac256d1a29671d06c4fa468cd0703f3
MusangQim <musangqim03@gmail.com>
2026-04-11 16:17:00 +0800

    update

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 import sys
 
+scores = []
+user_input = len(sys.argv)
+list_input = str(sys.argv) - 1
+print("=== Player Score Analytics ===")
+print(f"Score processed: {list_input}")
+
+if user_input > 1:
+    total_player = user_input - 1
+    print(f"Total players: {total_player}")
+else:
+    print("No score provided.")
