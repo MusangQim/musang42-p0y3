156bd14c92646ce2201535b7dda1ee6df5d6dce9
MusangQim <musangqim03@gmail.com>
2026-04-14 12:35:15 +0800

    update

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -8,11 +8,12 @@ for arg in sys.argv[1:]:
     try:
         score = int(arg)
         scores.append(score)
-        print(f"Scores processed: {scores}")
     except ValueError as e:
         print(f"Invalid parameter: {e}")
-        exit
-#print(f"Scores processed: {scores}")
+if scores:
+    print(f"Scores processed: {scores}")
+else:
+    print("No score provided.")
 if user_input > 1:
     total_player = user_input - 1
     print(f"Total players: {total_player}")
