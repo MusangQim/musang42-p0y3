2e9ffb248285511f61aad926e52f4bcf7fe939d2
MusangQim <musangqim03@gmail.com>
2026-04-16 18:44:59 +0800

    update

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -2,7 +2,18 @@
 import math
 
 def get_player_pos():
+    coords_input = input(float("Enter new coordinates as floats in format: 'x,y,z':"))
+    coords_split = coords_input.split(",")
+    if len(coords_split) != 3:
+        print("Invalid syntax")
+    else:
+
+
+def main():
+    print("=== Game Coordinate System ===")
+    print("Get a first set of coordinates")
+    get_player_pos()
+
 
 if __name__ == "__main__":
-    print("=== Game Coordinate System ===")
-    get_player_pos()
+    main()
