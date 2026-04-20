b77f3126e5c873f5bd9a5590ebe204f1bdc9427b
MusangQim <musangqim03@gmail.com>
2026-04-20 13:22:02 +0800

    :update

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 import sys
 
-inventory = {
-    "sword" : 1,
-    "shield" : 2,
-    "armor" : 3,
-    "helmet" : 1
+inventory = {}
+#    "sword" : 1,
+#   "shield" : 2,
+#    "armor" : 3,
+#    "helmet" : 1,
+#    "potion" : 1
 
-}
+def main():
+    for arg in sys.argv[1:]:
+        input_user = arg.split(':')
+        if len(input_user) != 2:
+            print("Invalid parameter")
+            continue
+        else:
+            print("Redundant item")
+            skip
+        try:
+            convert = int(input_user[1])
+        except ValueError:
+            print(f"Quantity Error for '{arg}:")
+
+if __name__ == "__main__":
+    print("=== Inventory System Analysis ===")
+    main()
