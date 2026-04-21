18f9dc197c8dd52844550cf95783429b34641bc3
MusangQim <musangqim03@gmail.com>
2026-04-22 02:15:35 +0800

    update: even 1 - 1000 can be display by name and action using tuple, yield, and next in main function

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
+import random
 import typing
 
-
-def gen_event() -> None:
-
-
-def consume_event() -> None:
+players = ["alice", "bob", "charlie", "dylan"]
+actions = ["run", "eat", "sleep", "grab", "swim", "move"]
 
 
-def main() -> None:
+def gen_event():
+    while True:
+        random_player = random.choice(players)
+        random_action = random.choice(actions)
+        yield (random_player, random_action)
+
+
+# def consume_event() -> None:
+
+
+def main():
+    event_gen = gen_event()
+    for i in range(1000):
+        event = next(event_gen)
+        name, action = event
+        print(f"Event {i}: Player {name} did action {action}")
 
 
 if __name__ == "__main__":
+    print("=== Game Data Stream Processor ===")
     main()
