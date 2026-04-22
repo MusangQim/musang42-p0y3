216c711d89f49a058753abc6b46a063eef660b85
MusangQim <musangqim03@gmail.com>
2026-04-22 21:57:45 +0800

    update

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -2,8 +2,13 @@
 
 import random
 
-def main() -> None:
 
-    
+def main() -> None:
+    names = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
+    print("=== Game Data Alchemist ===\n")
+    all_capitalize = [players.capitalize() for players in names]
+    print(all_capitalize)
+
+
 if __name__ == "__main__":
     main()
