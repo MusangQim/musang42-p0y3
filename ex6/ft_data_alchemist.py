e1c81666ade06ef4798a0589514f43a4fa03b779
MusangQim <musangqim03@gmail.com>
2026-04-23 13:25:25 +0800

    update

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -11,18 +11,20 @@ def main() -> None:
     print(f"Initial list of players: {names}")
     all_capital = [players.capitalize() for players in names]
     print(f"New list with all capitalized: {all_capital}")
-    only_capital = [player for player in names if player == player.capitalize()]
+    only_capital = [player for player in names
+                    if player == player.capitalize()]
     # or it can use:
     # only_capital = [player for player in names if player.istitle()]
     print(f"New list of capitalized names only: {only_capital}")
     print()
     # --- using Dictionary Comprehensive ---
-    score_dict = {name: random.randint(1, 1000) for name in all_capital}
-    print(f"Score dict: {score_dict}")
-    score_average = sum(score_dict.values()) / len(score_dict)
-    print(f"Score average is {score_average:.2f}")
-    score_high = {name: score for name, score in score_dict.items() if score > score_average}
-    print(f"High scores: {score_high}")
+    scores = {name: random.randint(1, 1000) for name in all_capital}
+    print(f"Score dict: {scores}")
+    average = sum(scores.values()) / len(scores)
+    print(f"Score average is {average:.2f}")
+    high = {name: score for name, score in scores.items()
+            if score > average}
+    print(f"High scores: {high}")
 
 
 if __name__ == "__main__":
