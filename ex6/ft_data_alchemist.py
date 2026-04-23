c84c95e99095857968b7918523d515efa61e7e27
MusangQim <musangqim03@gmail.com>
2026-04-23 13:15:44 +0800

    update

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -4,10 +4,25 @@ import random
 
 
 def main() -> None:
-    names = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
+    # --- using List Comprehensive ---
+    names = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma',
+             'Gregory', 'john', 'kevin', 'Liam']
     print("=== Game Data Alchemist ===\n")
-    all_capitalize = [players.capitalize() for players in names]
-    print(all_capitalize)
+    print(f"Initial list of players: {names}")
+    all_capital = [players.capitalize() for players in names]
+    print(f"New list with all capitalized: {all_capital}")
+    only_capital = [player for player in names if player == player.capitalize()]
+    # or it can use:
+    # only_capital = [player for player in names if player.istitle()]
+    print(f"New list of capitalized names only: {only_capital}")
+    print()
+    # --- using Dictionary Comprehensive ---
+    score_dict = {name: random.randint(1, 1000) for name in all_capital}
+    print(f"Score dict: {score_dict}")
+    score_average = sum(score_dict.values()) / len(score_dict)
+    print(f"Score average is {score_average:.2f}")
+    score_high = {name: score for name, score in score_dict.items() if score > score_average}
+    print(f"High scores: {score_high}")
 
 
 if __name__ == "__main__":
