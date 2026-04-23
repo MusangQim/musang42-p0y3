9cadba1eb9752debaa5294a2d8a09902a3c2bc0a
MusangQim <musangqim03@gmail.com>
2026-04-23 23:55:53 +0800

    update:fix small error

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import sys
 
-input_user = len(sys.argv)
-print("=== Command Quest ===")
-print(f"Program name: {sys.argv[0]}")
-if len(sys.argv) > 1:
-    input_receive = input_user - 1
-    print(f"Arguments received: {input_receive}")
-    for input_list in range(1, len(sys.argv)):
-        print(f"Argument {input_list}: {sys.argv[input_list]}")
-else:
-    print("No arguments provided!")
-print(f"Total arguments: {input_user}")
+
+def main() -> None:
+    input_user = len(sys.argv)
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    if len(sys.argv) > 1:
+        input_receive = input_user - 1
+        print(f"Arguments received: {input_receive}")
+        for input_list in range(1, len(sys.argv)):
+            print(f"Argument {input_list}: {sys.argv[input_list]}")
+    else:
+        print("No arguments provided!")
+    print(f"Total arguments: {input_user}")
+
+
+if __name__ == "__main__":
+    main()
